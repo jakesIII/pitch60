b6093869fbd7d4b2a2a91e37887539abbddcc24c
jakesIII <jayruma@yahoo.com>
2019-11-25 10:17:34 +0300

    render the registration form

--- app/auth/views.py
+++ app/auth/views.py
@@ -26,5 +26,7 @@ def register():
         db.session.commit()
 
         return redirect(url_for('auth.login'))
+        
         title = "New Account"
+
     return render_template('auth/register.html', registration_form = form)
