805647eca34447e1628ec5b52e8399c9b977bf00
jakesIII <jayruma@yahoo.com>
2019-11-26 01:24:32 +0300

    add model relationship

--- app/models.py
+++ app/models.py
@@ -18,7 +18,7 @@ class User(UserMixin, db.Model):
     bio=db.Column(db.String(255))
     avatar=db.Column(db.String())
     posts=db.Column(db.String())
-
+    posts=db.relationship('Posts' backref='user', lazy="dynamic")
     password_hash=db.Column(db.String(255))
 
 
@@ -44,7 +44,8 @@ class Posts(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     category=db.Column(db.String())
     description=db.Column(db.String())
-    user =db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comments=db.relationship('Comments', backref='post', lazy="dynamic")
 
     def save_post(self):
         db.session.add(self)
@@ -57,7 +58,7 @@ class Posts(db.Model):
 
     @classmethod
     def get_user_posts(cls, user):
-        user_posts=Posts.query.filter_by(user=user).all
+        user_posts=Posts.query.filter_by(user_id=user_id).all
         return user_posts
 
 
@@ -73,4 +74,14 @@ class Comments(db.Model):
 
     id=db.Column(db.Integer, primary_key=True)
     comment=db.Column(db.String)
-    posted_at=db.Column()
+    user = db.Column(db.String)
+    post_id=db.Column(db.Integer, db.ForeignKey('posts.id'))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls):
+        comments = Comments.query.filter_by(post_id).all()
+        return comments
