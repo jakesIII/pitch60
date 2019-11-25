1051fca4aaf1bf009b9ba2167c66040fad81c408
jakesIII <jayruma@yahoo.com>
2019-11-25 06:15:30 +0300

    instantiated the login manager

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,13 @@ from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 bootstrap=Bootstrap()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection='strong'
+logun_manager.login_view='auth.login'
 
 
 def create_app(config_name):
@@ -15,6 +19,7 @@ def create_app(config_name):
 
     bootstrap.init_app(app)
     db.init_app(app)
+    
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
